3d4b822b3f7895a24a3fc8abbaa572ee50760dc0
Soham Shinde <sohamsshinde04@gmail.com>
2023-07-14 10:39:41 +0400

    Added jump
    Jump

--- Assets/Scripts/Player_1_movement.cs
+++ Assets/Scripts/Player_1_movement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player_1_movement : MonoBehaviour
 {
     public float movementSpeed = 10f;
+    public float jumpforce = 5;
+    
+    public Rigidbody2D rb;
 
     void Start()
     {
@@ -17,5 +20,10 @@ public class Player_1_movement : MonoBehaviour
 
         Vector2 playerMovement = new Vector2(movementSpeed * inputx, 0) * Time.deltaTime;
         transform.Translate(playerMovement);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
     }
 }
--- Assets/Scripts/Player_2_movement.cs
+++ Assets/Scripts/Player_2_movement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player_2_movement : MonoBehaviour
 {
     public float movementSpeed = 10f;
+    public float jumpforce = 5;
+    public bool jumplimit = true;
+
+    public Rigidbody2D rb;
 
     void Start()
     {
@@ -17,5 +21,10 @@ public class Player_2_movement : MonoBehaviour
 
         Vector2 playerMovement = new Vector2(movementSpeed * inputx, 0) * Time.deltaTime;
         transform.Translate(playerMovement);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !jumplimit)
+        {
+            rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
     }
 }
