8b1f2115fcd5c207c0f7d6a71e4428a8a4ccfb1e
Soham Shinde <sohamsshinde04@gmail.com>
2023-07-23 15:16:07 +0400

    Crouch
    Added a crouch mechanic so that the player can hide in compact spaces and avoid getting detected

--- Assets/Scripts/Player_2_movement.cs
+++ Assets/Scripts/Player_2_movement.cs
@@ -8,10 +8,16 @@ public class Player_2_movement : MonoBehaviour
     public float jumpforce = 5;
     public bool jumplimit = true;
 
+    public Vector2 player2_Stand;
+    public Vector2 player2_Crouch;
+
+    public BoxCollider2D P2_boxCollider2D;
+
     public Rigidbody2D rb;
 
     void Start()
     {
+        P2_boxCollider2D = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
